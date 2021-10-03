5be0f0176133e31d15b5f648d81b31deb5ce493e
RanoJ15 <ranojuraeva78@gmail.com>
2021-10-03 17:14:49 -0400

    added Hello

--- src/tests/HelloWorldTest.java
+++ src/tests/HelloWorldTest.java
@@ -4,6 +4,8 @@ public class HelloWorldTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("One more commit");
+        System.out.println("Hello");
+
     }
 
 }
